14540b0abb20deaf144b538f940812ed2a959ced
Andersen <mizio.spatafora@gmail.com>
2021-11-19 16:15:22 +0100

    Solve unit test TestTotalPrice by implementing a price without promotion subtracted any accumulated qualified promotion

--- PromotionEngineLibrary/PromotionEngine.cs
+++ PromotionEngineLibrary/PromotionEngine.cs
@@ -8,15 +8,16 @@ public static class PromotionEngineLibrary
     public static int PriceC => 20;
     public static int PriceD => 15;
 
+    public static IEnumerable<int> Prices => new List<int>{PriceA, PriceB, PriceC, PriceD};
+
     public static int Promotion3As => 130;
-    public static int Promotion3AsSaving => PriceA*3 - 130;
+    public static int Promotion3AsSaving => PriceA*3 - Promotion3As;
     
     public static int Promotion2Bs => 45;
-    public static int Promotion2BsSaving => PriceB*2 - 45;
+    public static int Promotion2BsSaving => PriceB*2 - Promotion2Bs;
     
     public static int PromotionCandD => 30;
-    public static int PromotionCandDSaving => PriceC + PriceD - 30;
-
+    public static int PromotionCandDSaving => PriceC + PriceD - PromotionCandD;
 
     public static IEnumerable<int> CountSKU(this IEnumerable<string>? sku)
     {
@@ -43,10 +44,27 @@ public static class PromotionEngineLibrary
         return counts;
     }
 
-    public static int TotalPrice(this IEnumerable<int>? str)
+    public static int TotalPrice(this IEnumerable<int>? counts)
     {
+        // Todo: implement modular expression that uses promotion saving together with price without promotions.
+        // int priceWithoutPromotion = counts.ElementAt(0)*PriceA + counts.ElementAt(1)*PriceB;
         int priceWithoutPromotion = 0;
+        foreach (var ite in Enumerable.Range(0, counts.Count()))
+        {
+            priceWithoutPromotion += counts.ElementAt(ite)*Prices.ElementAt(ite);
+        }
 
-        return 0;
+        int Promotion3AsCount = counts.ElementAt(0)/3;
+        int Promotion3AsTotalSaving = Promotion3AsCount*Promotion3AsSaving;
+        
+        int Promotion2BsCount = counts.ElementAt(1)/2;
+        int Promotion2BsTotalSaving = Promotion2BsCount*Promotion2BsSaving;
+
+        int PromotionCandDCount = Math.Min(counts.ElementAt(2), counts.ElementAt(3));
+        int PromotionCandDTotalSaving = PromotionCandDCount*PromotionCandDSaving;
+
+        int totalPromotionSaving = Promotion3AsTotalSaving + Promotion2BsTotalSaving + PromotionCandDTotalSaving;
+
+        return priceWithoutPromotion - totalPromotionSaving;
     }
 }
