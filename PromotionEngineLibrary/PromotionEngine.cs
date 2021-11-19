a8bbd1d251c00e4cf91d7acbe9c6187fd988ec35
Andersen <mizio.spatafora@gmail.com>
2021-11-19 13:13:28 +0100

    Initial commit

---
+++ PromotionEngineLibrary/PromotionEngine.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Promotion.Engine.Library;
+public static class PromotionEngineLibrary
+{
+    private static int _priceA = 50;
+    private static int _priceB = 30;
+    private static int _priceC = 20;
+    private static int _priceD = 15;
+
+    public static IEnumerable<int> CountSKU(this IEnumerable<string>? str)
+    {
+        IEnumerable<int> counts = new List<int>{2, 0, 2, 0};
+        return counts;
+        // foreach(string i in str)
+        // {
+        //     switch(i)
+        //     {
+        //         case i.Equals("A"):
+        //             counts[0] += 1;
+        //             break;
+        //         case i.Equals("B"):
+        //             counts[1] += 1;
+        //             break;
+        //         case i.Equals("C"):
+        //             counts[2] += 1;
+        //             break;
+        //         case i.Equals("D"):
+        //             counts[3] += 1;
+        //             break;
+        //     }
+        // }
+    }
+
+    public static int TotalPrice(this IEnumerable<string>? str)
+    {
+        if (str.Contains("A"))
+            return 50;
+        else
+            return 0;
+    }
+
+}
---
+++ PromotionEngineLibraryTest/UnitTest1.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using Promotion.Engine.Library;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Promotion.Engine.Library.Test;
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void TestTotalPrice()
+    {
+        string[] stockKeepingUnits = {"A", "B", "A", "B"};
+        int expectedTotal = 45 + 2*50;
+        int totalPrice = stockKeepingUnits.TotalPrice();
+        bool result = expectedTotal == totalPrice;
+        Assert.IsTrue(result, String.Format("Expected total price '{0}': true, but actual price '{1}': {2}", expectedTotal, totalPrice, result));
+        // Assert.Pass();
+    }
+
+    [Test]
+    public void TestCountSKU()
+    {
+        IEnumerable<string> stockKeepingUnits = new List<string>{"A", "B", "A", "B"};
+        var counts = stockKeepingUnits.CountSKU();
+        IEnumerable<int> expected = new List<int>{2, 2, 0, 0};
+        bool result = counts.SequenceEqual(expected);
+        Assert.IsTrue(result, String.Format("Expected counts '{0}': true, but actual counts '{1}': {2}", String.Join(",", expected), String.Join(",", counts), result));
+    }
+
+
+}
