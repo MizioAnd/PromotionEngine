24a68fad900e2a0c68b47e887d807eb7fc2d7c42
Andersen <mizio.spatafora@gmail.com>
2021-12-02 17:46:05 +0100

    Implement INotifyPropertyChanged in viewmodel and raise an event whenever the setter of the Input variable gets called. Use the EventHandler delegate instead of a custom delegate.

--- PromotionEngineConsoleApp/PromotionEngineViewModel.cs
+++ PromotionEngineConsoleApp/PromotionEngineViewModel.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 
 
 namespace Promotion.Engine.ConsoleApp;
-public class PromotionEngineViewModel //: INotifyPropertyChanged
+public class PromotionEngineViewModel : INotifyPropertyChanged
 {
     private List<PromotionRule> _promotionRules = new List<PromotionRule>();
     private IEnumerable<string>? stockKeepingUnits;
@@ -16,6 +16,9 @@ public class PromotionEngineViewModel //: INotifyPropertyChanged
     {
         _totalPrice = 0;
         Add3PromotionRules();
+        // computeTotalPriceFor3RulesDel += this.ComputeTotalPriceFor3Rules;
+        // PropertyChanged += new PropertyChangedEventHandler(computeTotalPriceFor3RulesDel);
+        PropertyChanged += new PropertyChangedEventHandler(this.ComputeTotalPriceFor3Rules);
     }
 
     public string? Input 
@@ -23,16 +26,21 @@ public class PromotionEngineViewModel //: INotifyPropertyChanged
         get { return _input; }
         set 
         {
-            _input = value;
+            if (value != this.Input)
+            {
+                _input = value;
+                NotifyPropertyChanged();
+            }
+            // _input = value;
             // NotifyPropertyChanged();
-            computeTotalPriceFor3RulesDel += this.ComputeTotalPriceFor3Rules;
-            computeTotalPriceFor3RulesDel();
-            computeTotalPriceFor3RulesDel -= this.ComputeTotalPriceFor3Rules;
+            // computeTotalPriceFor3RulesDel += this.ComputeTotalPriceFor3Rules;
+            // computeTotalPriceFor3RulesDel();
+            // computeTotalPriceFor3RulesDel -= this.ComputeTotalPriceFor3Rules;
         } 
     }
 
-    public delegate void ComputeTotalPriceFor3RulesDelegate();
-    ComputeTotalPriceFor3RulesDelegate? computeTotalPriceFor3RulesDel;
+    // public delegate void ComputeTotalPriceFor3RulesDelegate(object? sender, PropertyChangedEventArgs e);
+    // ComputeTotalPriceFor3RulesDelegate? computeTotalPriceFor3RulesDel;
 
     public int TotalPrice
     {
@@ -46,12 +54,12 @@ public class PromotionEngineViewModel //: INotifyPropertyChanged
 
     public static IEnumerable<string> StockKeepingUnitsExample { get; } = new List<string>{"A", "A", "A", "B", "B", "B", "B", "B", "C", "D"};
 
-    // public event PropertyChangedEventHandler? PropertyChanged;
+    public event PropertyChangedEventHandler? PropertyChanged;
 
-    // private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
-    // {
-    //     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    // }
+    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 
     private void Add3PromotionRules()
     {
@@ -74,7 +82,7 @@ public class PromotionEngineViewModel //: INotifyPropertyChanged
         _promotionRules.CreatePromotion2ItemsForFixedPrice(item_i, item_j, price);
     }
 
-    private void ComputeTotalPriceFor3Rules()
+    private void ComputeTotalPriceFor3Rules(object? sender, PropertyChangedEventArgs e)
     {
         try {
             if (string.IsNullOrEmpty(_input))
@@ -83,9 +91,6 @@ public class PromotionEngineViewModel //: INotifyPropertyChanged
             _counts = stockKeepingUnits.CountSKU();
             _totalPrice = _counts.TotalPriceUsingPromotionRules(_promotionRules);
 
-        } catch (ArgumentNullException e) {
-            // if (string.IsNullOrEmpty(Input))
-            //     Environment.Exit(0);
-        }
+        } catch (ArgumentNullException) {}
     }
 }
