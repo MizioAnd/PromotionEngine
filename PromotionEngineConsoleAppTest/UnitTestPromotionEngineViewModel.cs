e49743515bbf412fedcd89ff0248e960699719f2
Andersen <mizio.spatafora@gmail.com>
2021-12-03 00:43:06 +0100

    Improve unit test such that expected totalPrice is computed using PromotionEngineLibrary to manually add 3 rules

--- PromotionEngineConsoleAppTest/UnitTestPromotionEngineViewModel.cs
+++ PromotionEngineConsoleAppTest/UnitTestPromotionEngineViewModel.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using System;
 using Promotion.Engine.ConsoleApp;
+using Promotion.Engine.Library;
+using System.Collections.Generic;
 
 namespace Promotion.Engine.ConsoleApp.UnitTests;
 public class Tests1
@@ -11,12 +13,36 @@ public class Tests1
     }
 
     [Test]
-    public void TestAdd3PromotionRules()
+    public void TestComputeTotalPriceFor3Rules()
     {   
-        // Todo: Test that 3 promotion rules have been added by checking lenght or total price
+        var input = "A,A,A,B,B,B,B,B,C,D";
+        IEnumerable<string> stockKeepingUnits = new List<string>(input.Split(","));
+        var counts = stockKeepingUnits.CountSKU();
+        
+        List<PromotionRule> PromotionRules = new List<PromotionRule>();
+
+        // Create Promotion rule
+        int price = 130;
+        int nItems = 3;
+        string item_i = "A";
+        PromotionRules.CreatePromotionNItemsForFixedPrice(nItems, item_i, price);
+
+        // Create Promotion rule
+        price = 45;
+        nItems = 2;
+        item_i = "B";
+        PromotionRules.CreatePromotionNItemsForFixedPrice(nItems, item_i, price);
+
+        // Create Promotion rule
+        price = 30;
+        item_i = "C";
+        string item_j = "D";
+        PromotionRules.CreatePromotion2ItemsForFixedPrice(item_i, item_j, price);
+
+        var expectedTotal = counts.TotalPriceUsingPromotionRules(PromotionRules);
+
         PromotionEngineViewModel promotionEngineViewModel = new PromotionEngineViewModel();
-        promotionEngineViewModel.Input = "A,A,A";
-        var expectedTotal = 130;
+        promotionEngineViewModel.Input = input;
         var totalPrice = promotionEngineViewModel.TotalPrice;
         var result = expectedTotal == totalPrice;
         Assert.IsTrue(result, String.Format("Expected total price '{0}': true, but actual price '{1}': {2}", expectedTotal, totalPrice, result));
