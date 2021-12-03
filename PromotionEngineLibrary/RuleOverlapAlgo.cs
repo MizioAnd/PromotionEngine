4c359e6ea4bd78b13f192482309a3ac266db2a6f
Andersen <mizio.spatafora@gmail.com>
2021-12-04 00:13:46 +0100

    Add class for overlap algo

---
+++ PromotionEngineLibrary/RuleOverlapAlgo.cs
@@ -0,0 +1,15 @@
+
+namespace Promotion.Engine.Library;
+public static class RuleOverlapAlgo
+{
+    public static IEnumerable<int> OptimizeRulesApplied(this IEnumerable<int> countsSKU)
+    {
+        return new List<int>();
+    }
+
+    public static int OverlappingPromotionRules(this IEnumerable<int> rulesAppliedCount)
+    {
+        return 1;
+    }
+    
+}
